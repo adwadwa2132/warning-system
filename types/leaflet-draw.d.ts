4d0b61175f668eb729b8875ba562d22af90050ac
Wilson <wilsonwu628@gmail.com>
2025-03-22 02:46:58 -0400

    Fix TypeScript issues for Netlify deployment

--- types/leaflet-draw.d.ts
+++ types/leaflet-draw.d.ts
@@ -1,50 +1,54 @@
-import * as L from 'leaflet';
+declare module 'leaflet-draw' {
+  import * as L from 'leaflet';
 
-declare module 'leaflet' {
-  namespace Control {
-    class Draw extends L.Control {
-      constructor(options?: DrawOptions);
+  namespace L {
+    namespace Draw {
+      namespace Event {
+        const CREATED: string;
+        const EDITED: string;
+        const DELETED: string;
+        const DRAWSTART: string;
+        const DRAWSTOP: string;
+        const DRAWVERTEX: string;
+        const EDITSTART: string;
+        const EDITMOVE: string;
+        const EDITRESIZE: string;
+        const EDITVERTEX: string;
+        const EDITSTOP: string;
+        const DELETESTART: string;
+        const DELETESTOP: string;
+      }
+      
+      interface DrawOptions {
+        polyline?: any;
+        polygon?: any;
+        rectangle?: any;
+        circle?: any;
+        marker?: any;
+        circlemarker?: any;
+      }
+      
+      interface EditOptions {
+        featureGroup: L.FeatureGroup;
+        edit?: any;
+        remove?: any;
+      }
     }
-  }
 
-  namespace Draw {
-    namespace Event {
-      const CREATED: string;
-      const EDITED: string;
-      const DELETED: string;
-      const DRAWSTART: string;
-      const DRAWSTOP: string;
-      const DRAWVERTEX: string;
-      const EDITSTART: string;
-      const EDITMOVE: string;
-      const EDITRESIZE: string;
-      const EDITVERTEX: string;
-      const EDITSTOP: string;
-      const DELETESTART: string;
-      const DELETESTOP: string;
+    namespace Control {
+      interface DrawConstructorOptions {
+        position?: L.ControlPosition;
+        draw?: Draw.DrawOptions;
+        edit?: Draw.EditOptions;
+      }
+      
+      class Draw extends L.Control {
+        constructor(options?: DrawConstructorOptions);
+      }
     }
-  }
 
-  interface DrawOptions {
-    position?: string;
-    draw?: {
-      polyline?: boolean | {
-        shapeOptions?: L.PolylineOptions;
-      };
-      polygon?: boolean | {
-        allowIntersection?: boolean;
-        showArea?: boolean;
-        shapeOptions?: L.PolylineOptions;
-      };
-      rectangle?: boolean | any;
-      circle?: boolean | any;
-      marker?: boolean | any;
-      circlemarker?: boolean | any;
-    };
-    edit?: {
-      featureGroup: L.FeatureGroup;
-      edit?: boolean;
-      remove?: boolean;
-    };
+    interface FeatureGroup extends L.FeatureGroup {
+      _drawnItems?: boolean;
+    }
   }
 } 
